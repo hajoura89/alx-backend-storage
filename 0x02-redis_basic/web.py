854ef08045d1c3002d2d6e088b2f4505b7fb82d5
hajoura89 <hajarkammou@gmail.com>
2024-02-28 23:08:44 +0000

    Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,62 +1,32 @@
-import requests
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
 import redis
+import requests
 from functools import wraps
-from time import time
-
-""" Connect to Redis server"""
-redis_client = redis.Redis()
+from typing import Callable
 
 
-def cache_with_expiry(expiration_time):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            cache_key = f"cache:{url}"
-            count_key = f"count:{url}"
-
-            """Check if the result is in the cache"""
-            cached_result = redis_client.get(cache_key)
-            if cached_result:
-                """If cached, increment the access count"""
-                redis_client.incr(count_key)
-                return cached_result.decode('utf-8')
-
-            """If not in cache, fetch from the web"""
-            result = func(*args, **kwargs)
-
-            """Cache the result with an expiration time"""
-            redis_client.setex(cache_key, expiration_time, result)
-
-            """Increment the access count"""
-            redis_client.incr(count_key)
-
-            return result
-
-        return wrapper
-    return decorator
+redis_store = redis.Redis()
+"""The Redis instance"""
 
 
-@cache_with_expiry(10)
-def get_page(url):
-    """Simulate a slow response using
-    slowwly.robertomurray.co.uk"""
-    response = requests.get(url)
-    return response.text
+def data_cacher(method: Callable) -> Callable:
+    """Caches the output of fetched data"""
+    @wraps(method)
+    def invoker(url) -> str:
+        """The wrapper function for caching the output"""
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-"""Example usage"""
-if __name__ == "__main__":
-    url = (
-        "http://slowwly.robertomurray.co.uk/delay/10000/"
-        "url/http://www.google.com"
-    )
-
-    """Access the page multiple times"""
-    for _ in range(5):
-        print(get_page(url))
-
-    """Print access count"""
-    count_key = f"count:{url}"
-    access_count = redis_client.get(count_key)
-    print(f"Access count for {url}: {int(access_count)}")
+@data_cacher
+def get_page(url: str) -> str:
+    """Returns the HTML content of a URL"""
+    return requests.get(url).text
