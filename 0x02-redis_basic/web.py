c95bfb7baa82acf8e8bea858474c9a6d9dcc9190
hajoura89 <hajarkammou@gmail.com>
2024-02-28 22:31:50 +0000

    Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,6 +3,8 @@
 import redis
 import requests
 from functools import wraps
+from typing import Callable
+
 
 redis_store = redis.Redis()
 
@@ -10,29 +12,24 @@ redis_store = redis.Redis()
 def data_cacher(method: Callable) -> Callable:
     """Caches the output of fetched data"""
     @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = redis_store.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        redis_store.incr(key_count)
-        redis_store.set(key, html_content, ex=10)
-        redis_store.expire(key, 10)
-        return html_content
-    return wrapper
+    def invoker(url) -> str:
+        """The wrapper function for caching the output"""
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
     """Returns the HTML content of a URL"""
     return requests.get(url).text
- 
+
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
